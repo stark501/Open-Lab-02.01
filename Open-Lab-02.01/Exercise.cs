8608903cf6ec5d1167efee437847f3ee705c2908
stark501 <mkudlacej@gmail.com>
2021-09-29 16:07:57 +0200

    update exercise

--- Open-Lab-02.01/Exercise.cs
+++ Open-Lab-02.01/Exercise.cs
@@ -6,12 +6,13 @@ namespace Open_Lab_02._01
     {
         public double[] Create5Numbers()
         {
-            throw new NotImplementedException();
+               double[] value= { 5,7,8,9,10 };
+                 return value;
         }
 
         public double GetSecond(double[] numbers)
         {
-            throw new NotImplementedException();
+              return numbers[1];
         }
     }
 }
